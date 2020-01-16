8802e744d0490fd00002fd6dcacbab0a860621ae
Damla Türker <damla@nevotek.com>
2020-01-16 16:06:49 +0300

    RabbitMQ

---
+++ RabbitExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Messages message = new Messages() { Name = "Damla", SurName = "Turker", Message = "Hello World" };
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Borsoft",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string newMessage = JsonConvert.SerializeObject(message);
+                var body = Encoding.UTF8.GetBytes(newMessage);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "Damla",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine($"Gönderilen kişi: {message.Name}-{message.SurName}");
+            }
+
+            Console.WriteLine("Send Message...");
+            Console.ReadLine();
+        }
+    }
+    public class Messages
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ RebbitMQ_Receive/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RebbitMQ_Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "Damla",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Messages person = JsonConvert.DeserializeObject<Messages>(message);
+                    Console.WriteLine($" Name: {person.Name} Surname:{person.SurName} [{person.Message}]");
+                };
+                channel.BasicConsume(queue: "Damla",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine("Mesaj Delivered");
+                Console.ReadLine();
+            }
+        }
+    }
+    public class Messages
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Message { get; set; }
+    }
+}
